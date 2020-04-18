0a43045152205362cb1ce214d93d915c795762d9
Sior Meir <siorm2310@gmail.com>
2020-04-18 10:24:44 +0300

    added partials and a menu view

--- orders/models.py
+++ orders/models.py
@@ -5,27 +5,35 @@ from django.db import models
 
 class CommonInfo(models.Model):
     name = models.CharField(max_length=32)
-    small_price = models.FloatField(blank=True)
+    small_price = models.FloatField(blank=True, null=True)
     large_price = models.FloatField()
 
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class ToppingsAndExtras(models.Model):
     name = models.CharField(max_length=32)
-    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Pizza(CommonInfo):
-    pizza_type = models.CharField(max_length=32)  # Regular of Sicilian
-    available_toppings = models.ManyToManyField(ToppingsAndExtras)
+    PIZZA_TYPES = [("REG", "Regular"), ("SIC", "Sicilian")]
+    pizza_type = models.CharField(
+        max_length=3, choices=PIZZA_TYPES, default="REG"
+    )  # Regular of Sicilian
+    available_toppings = models.ManyToManyField(ToppingsAndExtras, blank=True)
     max_toppings = models.IntegerField()
 
 
 class Sub(CommonInfo):
-    available_toppings = models.ManyToManyField(ToppingsAndExtras)
+    available_toppings = models.ManyToManyField(ToppingsAndExtras, blank=True)
 
 
 class OtherDish(CommonInfo):
-    pass
+    dish_type = models.CharField(max_length=32, default="")
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "menu.html")
+
+
+def menu(request):
+    pizzas = Pizza.objects.all()
+    subs = Sub.objects.all()
+    other_dishes = OtherDish.objects.all()
+    toppings = ToppingsAndExtras.objects.all()
+
+    menu_items = {
+        "pizzas": pizzas,
+        "subs": subs,
+        "other_dishes": other_dishes,
+        "toppings": toppings,
+    }
+    return render(request, "menu.html", context=menu_items)
