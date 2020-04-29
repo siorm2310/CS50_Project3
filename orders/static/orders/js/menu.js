9d321bfd00a0336376259445fe39dce7071f87ec
Sior Meir <siorm2310@gmail.com>
2020-04-29 14:12:20 +0300

    added DB to tracking. changes to menu.js

--- orders/static/orders/js/menu.js
+++ orders/static/orders/js/menu.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {});
+function addToOrder(params) {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".pizza").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const dataId = btn.getAttribute("data-id");
+      console.log(dataId);
+    });
+  });
+
+  document.querySelectorAll(".sub").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const dataId = btn.getAttribute("data-id");
+      console.log(dataId);
+    });
+  });
+
+  document.querySelectorAll(".dish").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const dataId = btn.getAttribute("data-id");
+      console.log(dataId);
+    });
+  });
+});
