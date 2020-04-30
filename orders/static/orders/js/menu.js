2c46679d4a7ab3b9299a6347552959bdea9575e4
Sior Meir <siorm2310@gmail.com>
2020-04-30 22:47:31 +0300

    i don't know

--- orders/static/orders/js/menu.js
+++ orders/static/orders/js/menu.js
@@ -1,17 +1,6 @@
 function openItemMenu(itemId) {
-  const template = `
-  <div class="form-group">
-  <h4>Please select your toppings:</h4>
-  <br />
-  <strong>x toppings remain</strong>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Add to order</button>
-</div>
-
-  `;
+  const template = ``;
+  document.getElementById("item_form").innerHTML = template;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
