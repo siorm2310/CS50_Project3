b30f5b06118df54ce02de9219c1a848174347d3b
סיאור מאיר <siorm2310@gmail.com>
2020-05-01 22:40:00 +0300

    added JSON variables to menu.js

--- orders/static/orders/js/menu.js
+++ orders/static/orders/js/menu.js
@@ -1,5 +1,23 @@
+var pizzas = JSON.parse(document.getElementById("pizzasJSON").textContent);
+var subs = JSON.parse(document.getElementById("subsJSON").textContent);
+var dishes = JSON.parse(
+  document.getElementById("other_dishesJSON").textContent
+);
+var toppings = JSON.parse(document.getElementById("toppingsJSON").textContent);
+
 function openItemMenu(itemId) {
-  const template = ``;
+  const template = `
+  <div class="form-group">
+  <h4>Please select your toppings:</h4>
+  <br />
+  <strong>x toppings remain</strong>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Add to order</button>
+</div>
+`;
   document.getElementById("item_form").innerHTML = template;
 }
 
